0a1386134a4ab897f2c92f04c065b0dba5b3ae6f
siaym <133095845+siaym@users.noreply.github.com>
2023-05-17 21:44:38 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+// Created by siyam
+
+function menubar(){
+    let x = document.getElementById("navbar");
+    if ( x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
